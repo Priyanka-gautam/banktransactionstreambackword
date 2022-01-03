20b214340b16c43cfd3543ef0550e4bcefebae37
unknown <Priyanka@NTP-LAP-211.chn.nestgroup.net>
2022-01-03 12:44:50 +0530

    First commit

---
+++ src/test/java/com/banking/banktransactionstreambackword/BanktransactionstreambackwordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.banktransactionstreambackword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanktransactionstreambackwordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
